52b3c1bc66eb23aee65e4f3da6ba580eba22a67e
MariaPas235 <151913868+MariaPas235@users.noreply.github.com>
2024-11-22 17:47:15 +0100

    modelo creado

---
+++ src/main/java/org/example/Model/Desarrollador.java
@@ -0,0 +1,63 @@
+package org.example.Model;
+
+import java.util.Objects;
+
+public class Desarrollador {
+    private int id_desarrollador;
+    private String nombre;
+    private String pais;
+
+    public Desarrollador(int id_desarrollador, String nombre, String pais) {
+        this.id_desarrollador = id_desarrollador;
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    public Desarrollador() {}
+
+    public int getId_desarrollador() {
+        return id_desarrollador;
+    }
+
+    public void setId_desarrollador(int id_desarrollador) {
+        this.id_desarrollador = id_desarrollador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Desarrollador that = (Desarrollador) o;
+        return id_desarrollador == that.id_desarrollador;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id_desarrollador);
+    }
+
+    @Override
+    public String toString() {
+        return "Desarrollador{" +
+                "id_desarrollador=" + id_desarrollador +
+                ", nombre='" + nombre + '\'' +
+                ", pais='" + pais + '\'' +
+                '}';
+    }
+}
--- src/main/java/org/example/Model/Usuario.java
+++ src/main/java/org/example/Model/Usuario.java
@@ -1,17 +1,87 @@
 package org.example.Model;
 
+import java.util.Objects;
+
 public class Usuario {
-    private int id_Usuario;
+   private int id_usuario;
+   private String nombre;
+   private String email;
+   private String contrasena;
+   private boolean administrador;
 
-    public int getId_Usuario() {
-        return id_Usuario;
-    }
+   public Usuario(int id_usuario, String nombre, String email, String contrasena, boolean administrador) {
+      this.id_usuario = id_usuario;
+      this.nombre = nombre;
+      this.email = email;
+      this.contrasena = contrasena;
+      this.administrador = administrador;
+   }
 
-    public Usuario(int id_Usuario) {
-        this.id_Usuario = id_Usuario;
-    }
+   public Usuario(int id_usuario) {
+      this.id_usuario = id_usuario;
+   }
 
-    public void setId_Usuario(int id_Usuario) {
-        this.id_Usuario = id_Usuario;
-    }
+   public int getId_usuario() {
+      return id_usuario;
+   }
+
+   public void setId_usuario(int id_usuario) {
+      this.id_usuario = id_usuario;
+   }
+
+   public String getNombre() {
+      return nombre;
+   }
+
+   public void setNombre(String nombre) {
+      this.nombre = nombre;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getContrasena() {
+      return contrasena;
+   }
+
+   public void setContrasena(String contrasena) {
+      this.contrasena = contrasena;
+   }
+
+   public boolean isAdministrador() {
+      return administrador;
+   }
+
+   public void setAdministrador(boolean administrador) {
+      this.administrador = administrador;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Usuario usuario = (Usuario) o;
+      return Objects.equals(email, usuario.email);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hashCode(email);
+   }
+
+   @Override
+   public String toString() {
+      return "Usuario{" +
+              "id_usuario=" + id_usuario +
+              ", nombre='" + nombre + '\'' +
+              ", email='" + email + '\'' +
+              ", contrasena='" + contrasena + '\'' +
+              ", administrador=" + administrador +
+              '}';
+   }
 }
