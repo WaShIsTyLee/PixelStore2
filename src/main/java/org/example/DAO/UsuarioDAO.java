edc6c9a75e804290ccfea1184bf356c9882999ef
= <=>
2024-11-23 16:41:24 +0100

    cambios del gitignore

--- src/main/java/org/example/DAO/UsuarioDAO.java
+++ src/main/java/org/example/DAO/UsuarioDAO.java
@@ -10,9 +10,9 @@ public class UsuarioDAO {
     private final static String DELETE = "DELETE FROM usuario  WHERE id_usuario = ?";
 
     public Usuario delete(Usuario child) {
-        if (child != null || child.getId_Usuario() > 0) {
+        if (child != null || child.getId_usuario() > 0) {
             try (PreparedStatement pst = ConnectionDB.getConnection().prepareStatement(DELETE)) {
-                pst.setInt(1, child.getId_Usuario());
+                pst.setInt(1, child.getId_usuario());
                 pst.executeUpdate();
             } catch (SQLException e) {
                 e.printStackTrace();
